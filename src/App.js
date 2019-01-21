461b02fb082e9ecad463cf3622fe0fa59571f5ba
gordon <gordon.song@thoughtworks.com>
2019-01-21 16:40:46 +0800

    first implementation of the password strength checker component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./PasswordStrengthMeter";
+import "./App.css";
+import PasswordStrengthMeter from "./PasswordStrengthMeter";
 
 class App extends Component {
+  state = {
+    password: ""
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="meter">
+          <input
+            autoComplete="off"
+            type="password"
+            onChange={e => this.setState({ password: e.target.value })}
+          />
+          <PasswordStrengthMeter password={this.state.password} />
+        </div>
       </div>
     );
   }
