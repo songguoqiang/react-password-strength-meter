d109259872f9fad3221d57ac4cf6de27cfb2b463
gordon <gordon.song@thoughtworks.com>
2019-01-21 17:07:53 +0800

    refactor a component to functional component

--- src/PasswordStrengthMeter.jsx
+++ src/PasswordStrengthMeter.jsx
@@ -1,48 +1,45 @@
-import React, { Component } from "react";
+import React from "react";
 import "./PasswordStrengthMeter.css";
 import zxcvbn from "zxcvbn";
 
-class PasswordStrengthMeter extends Component {
-  createPasswordLabel = result => {
-    switch (result.score) {
-      case 0:
-        return "Weak";
-      case 1:
-        return "Weak";
-      case 2:
-        return "Fair";
-      case 3:
-        return "Good";
-      case 4:
-        return "Strong";
-      default:
-        return "Weak";
-    }
-  };
-
-  render() {
-    const { password } = this.props;
-    const testedResult = zxcvbn(password);
-    const passwordStrength = this.createPasswordLabel(testedResult);
-
-    return (
-      <div className="password-strength-meter">
-        <progress
-          className={`password-strength-meter-progress strength-${passwordStrength}`}
-          value={testedResult.score}
-          max="4"
-        />
-        <br />
-        <label className="password-strength-meter-label">
-          {password && (
-            <>
-              <strong>Password strength:</strong> {passwordStrength}
-            </>
-          )}
-        </label>
-      </div>
-    );
+function createPasswordLabel(result) {
+  switch (result.score) {
+    case 0:
+      return "Weak";
+    case 1:
+      return "Weak";
+    case 2:
+      return "Fair";
+    case 3:
+      return "Good";
+    case 4:
+      return "Strong";
+    default:
+      return "Weak";
   }
+}
+
+function PasswordStrengthMeter({ password }) {
+  const testedResult = zxcvbn(password);
+  const passwordStrength = createPasswordLabel(testedResult);
+
+  return (
+    <div className="password-strength-meter">
+      <progress
+        className={`password-strength-meter-progress strength-${passwordStrength}`}
+        value={testedResult.score}
+        max="4"
+      />
+      <br />
+      <label className="password-strength-meter-label">
+        {password && (
+          <>
+            <strong>Password strength:</strong> {passwordStrength}
+          </>
+        )}
+      </label>
+    </div>
+  );
 }
 
 export default PasswordStrengthMeter;
